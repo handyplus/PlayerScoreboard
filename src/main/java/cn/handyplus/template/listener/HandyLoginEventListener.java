6dc912db63949bc8afd4d3f56813d7baafe35a60
handy <handyplus@163.com>
2025-12-24 13:34:34 +0800

    升jar

--- src/main/java/cn/handyplus/template/listener/HandyLoginEventListener.java
+++ src/main/java/cn/handyplus/template/listener/HandyLoginEventListener.java
@@ -1,11 +1,11 @@
 package cn.handyplus.template.listener;
 
 import cn.handyplus.lib.annotation.HandyListener;
+import cn.handyplus.lib.internal.HandyLoginEvent;
 import cn.handyplus.lib.util.HandyHttpUtil;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
  * 登录事件
@@ -13,7 +13,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
  * @author handy
  */
 @HandyListener
-public class PlayerJoinEventListener implements Listener {
+public class HandyLoginEventListener implements Listener {
 
     /**
      * op 进入服务器发送更新提醒
@@ -21,7 +21,7 @@ public class PlayerJoinEventListener implements Listener {
      * @param event 事件
      */
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onOpPlayerJoin(PlayerJoinEvent event) {
+    public void onOpPlayerJoin(HandyLoginEvent event) {
         HandyHttpUtil.checkVersion(event.getPlayer());
     }
 
